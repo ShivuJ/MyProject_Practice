ca525ea8e18e0dc23760e2183008191e0e0ea579
ShivaniJadhav <97583560+ShivuJ@users.noreply.github.com>
2024-07-03 22:22:21 +0530

    Added diff folders

--- src/main/java/com/example/myproject/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.myproject.repository;
-
-import com.example.myproject.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
